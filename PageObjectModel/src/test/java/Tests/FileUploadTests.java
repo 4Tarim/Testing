8d6a5a111fb1be3f1818f7c2d6cc091aba0d3d4b
4Tarim <abdurahman.tevekkul@gmail.com>
2021-01-07 15:22:25 +0100

    added new tests, added new test classes to test package

---
+++ PageObjectModel/src/test/java/Tests/FileUploadTests.java
@@ -0,0 +1,40 @@
+package Tests;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import UtilityLibraries.Base;
+
+public class FileUploadTests extends Base {
+
+	
+	@Test
+	public void fileUploadTest1() {
+		driver.get("https://html.com/input-type-file/");
+		//assertTrue(false);
+		selLibrary.customWait(3);
+		selLibrary.uploadFile("src/test/resources/drivers/chromedriver.exe", By.id("fileupload"));
+		selLibrary.customWait(8);
+	}
+	
+	@Test
+	public void fileUploadTest2() {
+		//assertTrue(false);
+		driver.get("https://html.com/input-type-file/");		
+		selLibrary.customWait(3);
+		selLibrary.uploadFile("src/test/resources/drivers/chromedriver.exe", By.id("fileupload"));
+		selLibrary.customWait(8);
+	}
+	
+	@Test
+	public void fileUploadTest3() {
+		driver.get("https://html.com/input-type-file/");		
+		selLibrary.customWait(3);
+		selLibrary.uploadFile("src/test/resources/drivers/chromedriver.exe", By.id("fileupload"));
+		//assertTrue(false);
+		selLibrary.customWait(8);
+	}
+	
+}
---
+++ PageObjectModel/src/test/java/Tests/RemoteTests.java
@@ -0,0 +1,36 @@
+package Tests;
+
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class RemoteTests {
+
+	@Test
+	public void runRemoteTest()  {
+		WebDriver driver = null;
+		try {
+		String baseURL = "http://www.google.com/";
+		String nodeURL = "http://172.31.13.184:4444/wd/hub";
+		DesiredCapabilities cap = new DesiredCapabilities();
+		//InternetExplorerOptions op = new InternetExplorerOptions();
+		ChromeOptions op = new ChromeOptions();
+		//FirefoxOptions op = new FirefoxOptions();
+		
+		//op.setHeadless(true);
+		op.merge(cap);		
+		driver = new RemoteWebDriver(new URL(nodeURL), op);
+		
+		driver.get(baseURL);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		driver.close();
+		driver.quit();
+	}
+	
+}
