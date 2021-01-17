d8880c8e975db200cd14669df9309cc098219077
4Tarim <abdurahman.tevekkul@gmail.com>
2021-01-17 16:14:00 +0100

    deleting some libraries from global selenium lib class

--- PageObjectModel/src/test/java/utilityLibraries/GlobalSeleniumLibrary.java
+++ PageObjectModel/src/test/java/utilityLibraries/GlobalSeleniumLibrary.java
@@ -14,7 +14,6 @@ import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -855,12 +854,11 @@ public class GlobalSeleniumLibrary {
 			}
 		}
 	}
-
+/*
 	public static void main(String[] args) {
 		GlobalSeleniumLibrary myObject = new GlobalSeleniumLibrary();
-		myObject.autoAttachErrorImgToEmail();
+		myObject.autoAttachErrorImgToEmail();*/
 
 	}
 
-}
 
